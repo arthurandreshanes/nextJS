6a4be2f546807294adbb5d88fcf0bf1769bff857
Arthur Andrés Chamaco Hånes <arthurandres@Arthur-Andres-Chamaco-Hanes-sin-Mac.local>
2025-10-22 10:26:37 +0200

    Initial commit

---
+++ app/ui/fonts.ts
@@ -0,0 +1,10 @@
+import { warmFlightResponse } from 'next/dist/server/app-render/app-render';
+import { Inter, Lusitana } from 'next/font/google';
+
+export const inter = Inter({ 
+    subsets: ['latin'] });
+
+export const lusitana = Lusitana({
+    weight: ['400', '700'],
+    subsets: ['latin']
+})
